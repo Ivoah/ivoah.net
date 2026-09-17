f670283c200d1666dccd6c6dfd67632cec737954
Noah Rosamilia <noah@ivoah.net>
2026-09-17 19:15:38 -0400

    Add mondrian customizer

--- src/Endpoints.scala
+++ src/Endpoints.scala
@@ -8,12 +8,20 @@ class Endpoints(sitesPath: Path) {
     case ("GET" , "/", _) =>
       val sites = sitesPath.toFile.listFiles.map(_.getName).sorted.toSeq
       Response(Templates.root(sites))
-    case ("GET", "/mondrian", r) =>
+
+    case ("GET", "/mondrian.svg", r) =>
       val w = r.params.get("w").flatMap(_.toIntOption).getOrElse(512)
       val h = r.params.get("h").flatMap(_.toIntOption).getOrElse(512)
       val lineWidth = r.params.get("lineWidth").flatMap(_.toIntOption).getOrElse(5)
       val colors = r.params.get("colors").map(_.split(",").toSeq).getOrElse(Seq("white", "red", "blue", "yellow"))
       Response(mondrian(w, h, lineWidth, colors).render, headers = Map("Content-Type" -> Seq("image/svg+xml")))
+
+    case ("GET", "/mondrian", _) => Response(Templates.mondrian(512, 512, 5, Seq("white", "red", "blue", "yellow"), None, None))
+    case ("POST", "/mondrian", r) =>
+      r.form.expect("w", "h", "lineWidth", "colors", "adding", "removing")(Templates.mondrian)
+        .map(Response.apply)
+        .getOrElse(Response.BadRequest())
+
     case ("GET", s"/static/$file", _) => Response.forFile(Paths.get("static"), Paths.get(file))
   }
 }
--- src/Templates.scala
+++ src/Templates.scala
@@ -2,17 +2,22 @@ package net.ivoah.root
 
 import scalatags.Text.all.*
 import scalatags.Text.tags2.title
+import java.net.URLEncoder
 
 object Templates {
-  def root(sites: Seq[String]) = doctype("html")(html(
+  def page(_title: String)(_body: Frag*): String = doctype("html")(html(
     head(
       title(s"Noah Rosamilia"),
 
       link(rel:="stylesheet", href:="/static/style.css"),
       link(rel:="shortcut icon", `type`:="image/jpeg", href:="/static/favicon.jpg"),
     ),
-    body(
-      img(src:="/mondrian?w=256&h=256"),
+    body(_body)
+  )).render
+
+  def root(sites: Seq[String]) = page("Noah Rosamilia")(
+    div(cls:="center",
+      img(src:="/mondrian.svg?w=256&h=256"),
       div(id:="name", "Noah", br(), "Rosamilia"),
       div(id:="fan",
         for ((site, i) <- sites.zipWithIndex) yield {
@@ -20,5 +25,29 @@ object Templates {
         }
       )
     )
-  )).render
+  )
+
+  def mondrian(w: Int, h: Int, lineWidth: Int, colors: Seq[String], adding: Option[Boolean], removing: Option[Int]): String = {
+    val newColors = colors.zipWithIndex.collect {
+      case (c, i) if !removing.exists(_ == i) => c
+    } ++ (if (adding.exists(identity)) Some("white") else None)
+    page("Mondrian art generator")(
+      form(method:="POST", textAlign.left,
+        label("Width: ", input(`type`:="number", name:="w", value:=w)), br(),
+        label("Height: ", input(`type`:="number", name:="h", value:=h)), br(),
+        label("Line width: ", input(`type`:="number", name:="lineWidth", value:=lineWidth)), br(),
+        "Colors: ", ul(
+          for ((color, i) <- newColors.zipWithIndex) yield li(button(name:="removing", value:=i, raw("&times;")), input(`type`:="color", name:="colors", value:=color)),
+          li(button(name:="adding", value:="true", "Add"))
+        ),
+        button("Generate")
+      ),
+      img(src:=s"/mondrian.svg?${Seq(
+        "w" -> w,
+        "h" -> h,
+        "lineWidth" -> lineWidth,
+        "colors" -> newColors.mkString(",")
+      ).map{case (k, v) => s"$k=${URLEncoder.encode(v.toString, "utf-8")}"}.mkString("&")}")
+    )
+  }
 }
